5ba711922f98405ed266cc3416b93e8e1f22fcf3
vandersonsinet <vanderson.sinet@gmail.com>
2024-04-15 21:51:55 -0300

    atualizacao

--- model/despesa.py
+++ model/despesa.py
@@ -1,32 +1,28 @@
-from sqlalchemy import Column, String, Integer, DateTime, Float
+from sqlalchemy import Column, String, Integer, DateTime, Float, ForeignKey
 from datetime import datetime
 from typing import Union
+from sqlalchemy.orm import relationship
 
 from  model import Base
 
 class Despesa(Base):
     __tablename__ = 'despesa'
 
-    id = Column("pk_despesa", Integer, primary_key=True)
+    id = Column("id", Integer, primary_key=True)
     descricao = Column(String(140), unique=True)
     quantidade = Column(Integer)
     valor = Column(Float)
     data_insercao = Column(DateTime, default=datetime.now())
+    tipo_despesa_id = Column(Integer, ForeignKey("tipo_despesa.id"), nullable=False)
+    tipo_despesa = relationship("TipoDespesa", back_populates="despesa")
 
-    def __init__(self, descricao:str, quantidade:int, valor:float,
+    def __init__(self, descricao:str, quantidade:int, valor:float, tipo_despesa_id:int,
                  data_insercao:Union[DateTime, None] = None):
-        """
-        Cria uma Despesa
-
-        Arguments:
-            descricao: descrição da despesa.
-            quantidade: quantidade da despesa a ser inserida no banco
-            valor: valor da despesa
-            data_insercao: data de quando a despesa foi inserida
-        """
+        
         self.descricao = descricao
         self.quantidade = quantidade
         self.valor = valor
+        self.tipo_despesa_id = tipo_despesa_id
 
         # se não for informada, será o data exata da inserção no banco
         if data_insercao:
